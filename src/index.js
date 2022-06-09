0c1b2005320c5b20d7837783019e1d02afe4e4e5
AbhiInventam <abhishek@inventam.com>
2022-06-09 16:03:19 +0530

    Users Table with Redux Store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
 
 // v18
 // --------------------------------------------------------------------
@@ -18,9 +20,11 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
